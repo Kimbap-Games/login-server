1a97dbecf20e3c6b81cabbb83f30c1d8c35e5f97
vbn930 <vbn930@naver.com>
2025-10-09 10:33:34 -0700

    check

--- LoginServer/Program.cs
+++ LoginServer/Program.cs
@@ -11,8 +11,6 @@ using LoginServer.Azure;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
